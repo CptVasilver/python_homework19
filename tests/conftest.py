e952b060784b8c678f4f05afdd3d7690167ee985
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-04-23 16:37:08 +0300

    homework done

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from appium.options.android import UiAutomator2Options
+from selene import browser
+import os
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management_android():
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+
+        # Set URL of the application under test
+        "app": "bs://sample.app",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+
+            # Set your access credentials
+            "userName": "mike_Z6KI9R",
+            "accessKey": "2xS3HkxEMPu8MNUZSVYE"
+        }
+    })
+    browser.config.driver_remote_url = "http://hub.browserstack.com/wd/hub"
+    browser.config.driver_options = options
+    browser.config.timeout = float(os.getenv('timeout', '3.0'))
+
+    yield
+
+    browser.quit()
---
+++ tests/test_wikipedia_app.py
@@ -0,0 +1,14 @@
+from allure_commons._allure import step
+from selene import browser, have
+from appium.webdriver.common.appiumby import AppiumBy
+
+def test_search():
+    with step("Search Pulp Fiction"):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Pulp Fiction")
+    with step("Check what we found"):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text("Pulp Fiction"))
+    with step("Step into topic"):
+        results.first.click()
